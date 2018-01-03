3a53339e229d01e92f187361458dd81af6619868
muhammadsaripuddin <aripedward339@gmail.com>
2018-01-03 21:07:30 +0700

    membuat variabel

---
+++ tugas.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+	
+	
+	double a1[5];
+	double a2[5]; 
+	double a3[5];
+	
+	double Ra1[5];
+	double Ra2[5];
+	double Ra3[5];
+	
+	double RWa1[5];
+	double RWa2[5];
+	double RWa3[5];
+		
+	double w[5];
+	double m[5];
+	double ha[3];
+
+}
